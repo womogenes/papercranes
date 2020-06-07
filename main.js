b766bbf261d8a3e33c561201735ecd0119b233fd
Creonalia <chendi.luo@gmail.com>
2020-06-06 18:35:01 -0700

    cleaning up start page js

--- main.js
+++ main.js
@@ -1,26 +1,17 @@
 
 // Really basic JS but needs to be stored in a separate file.
-
-var craneImageEl;
-var theme;
-
 document.addEventListener("DOMContentLoaded", function(event) {
-  
-  craneImageEl = document.getElementById("craneImage");
-
   loadTheme();
-  
   resizeCraneImage();
   window.addEventListener("resize", resizeCraneImage);
 });
 
-
 function resizeCraneImage() {
   var width = document.documentElement.clientWidth / 2;
   var height = document.documentElement.clientHeight / 2;
-  
   var imgSize = Math.max(Math.min(width, height, 300), 50);
-  
+
+  var craneImageEl = document.getElementById("craneImage");
   craneImageEl.width = imgSize;
   craneImageEl.height = imgSize;
 }
