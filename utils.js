35f46ffd9814e7464f2fc3d9f1883a0b7cf3f95d
William Feng <williamfeng1729@gmail.com>
2020-06-21 13:18:47 -0700

    Fix title casing

--- utils.js
+++ utils.js
@@ -175,9 +175,7 @@ function spellf(userInput) {
 }
 
 String.prototype.toTitleCase = function () {
-  return this.replace(/\w\S*/g, function (txt) {
-    return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
-  });
+  return this.charAt(0).toUpperCase() + this.substr(1).toLowerCase();
 };
 
 String.prototype.camelize = function () {
