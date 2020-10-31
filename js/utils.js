f5d4fc35408c85c37572ed6e6194b8b866c14391
Creonalia <hyper.pyromaniac@simplelogin.co>
2020-10-30 22:18:41 -0700

    remove camelize, decamelize, titlecase from string.prototype

--- js/utils.js
+++ js/utils.js
@@ -173,21 +173,21 @@ function spellf(userInput) {
   }
 }
 
-toTitleCase = function () {
-  return this.charAt(0).toUpperCase() + this.substr(1).toLowerCase();
+toTitleCase = function (str) {
+  return str.charAt(0).toUpperCase() + str.substr(1).toLowerCase();
 };
 
-camelize = function () {
-  return this.replace(/^([A-Z])|[\s-_]+(\w)/g, function (match, p1, p2, offset) {
+camelize = function (str) {
+  return str.replace(/^([A-Z])|[\s-_]+(\w)/g, function (match, p1, p2, offset) {
     if (p2) return p2.toUpperCase();
     return p1.toLowerCase();
   });
 };
 
-decamelize = function (separator) {
+decamelize = function (str, separator) {
   separator = typeof separator === 'undefined' ? ' ' : separator;
 
-  return this
+  return str
     .replace(/([a-z\d])([A-Z])/g, '$1' + separator + '$2')
     .replace(/([A-Z]+)([A-Z][a-z\d]+)/g, '$1' + separator + '$2')
     .toLowerCase();
